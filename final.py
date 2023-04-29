7d355375cefb19be2c8b01631bfbbec5919ccc1a
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-04-29 04:35:23 +0200

    Update final.py

--- final.py
+++ final.py
@@ -3,6 +3,11 @@ import pandas as pd
 import streamlit as st
 
 # create a connection to the database
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+# create a connection to the database
 conn = sqlite3.connect('ecsel_database.db')
 query = "SELECT Country, Acronym FROM countries"
 df_countries = pd.read_sql(query, conn) 
@@ -15,10 +20,13 @@ print(country_dict)
 
 st.selectbox('Select a Country:',df_countries["Acronym"]) 
 
+
+
 conn = sqlite3.connect('ecsel_database.db')
-new_participants = '''SELECT shortName, name, activityType, organizationURL, SUM(ecContribution) 
+new_participants = '''SELECT country, shortName, name, activityType, organizationURL, SUM(ecContribution) 
            FROM participants
-           GROUP BY shortName'''
+           GROUP BY country
+           ORDER BY SUM(ecContribution) DESC'''
 df_participants = pd.read_sql_query(new_participants, conn)
 
 conn.close()
