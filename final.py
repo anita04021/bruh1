c30d1930baba3e975dc3b3a37dc356f47872635e
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-04-29 04:30:11 +0200

    Update final.py

--- final.py
+++ final.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 27 14:44:36 2023
-
-@author: anitabaculima
-"""
 import sqlite3
 import pandas as pd
 import streamlit as st
@@ -18,25 +11,19 @@ conn.close()
 
 country_dict = dict(zip(df_countries["Acronym"], df_countries["Country"]))
 
-print(country_dict) 
+print(country_dict)
+
+st.selectbox('Select a Country:',df_countries["Acronym"]) 
 
 conn = sqlite3.connect('ecsel_database.db')
-new_participants = '''SELECT country, shortName, name, activityType, organizationURL, SUM(ecContribution) 
+new_participants = '''SELECT shortName, name, activityType, organizationURL, SUM(ecContribution) 
            FROM participants
-           ORDER BY SUM(ecContribution) DESC'''
+           GROUP BY shortName'''
 df_participants = pd.read_sql_query(new_participants, conn)
-df_participants = df_participants.rename(columns={'country': 'Acronym'})
 
 conn.close()
-
 print(df_participants)
 
-options = df_participants['Acronym'].unique().tolist()
-selected_country = st.selectbox('Select a Country:', options) 
-filtered_df = df_participants[df_participants['Acronym'] == selected_country]
-
-st.write('Filtered dataframe:')
-st.write(filtered_df)
-
+st.dataframe(df_participants) 
 
                     
