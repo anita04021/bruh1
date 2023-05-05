5e0e8a6e9e1f05a5afa5322c05e1fc127a41e842
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-05-05 20:41:04 +0200

    Add files via upload

---
+++ final (1).py
@@ -0,0 +1,61 @@
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+# create a connection to the database
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+# create a connection to the database
+conn = sqlite3.connect('ecsel_database.db')
+query = "SELECT Country, Acronym FROM countries"
+df_countries = pd.read_sql(query, conn) 
+
+conn.close()
+
+print(df_countries)
+country_names = dict(zip(df_countries["Country"], df_countries["Acronym"]))
+
+print(country_names)
+
+selectedcountry = st.selectbox('Select a Country:',list(country_names.keys()))   # new step get acronym given the country name 
+selectedacronym = country_names[selectedcountry]
+
+
+# 2.8 create a new dataframe of participants
+conn = sqlite3.connect('ecsel_database.db')
+new_participants = '''SELECT country, shortName, name, activityType, organizationURL, role, SUM(ecContribution) 
+           FROM participants
+           WHERE role = 'participant'AND 'country'='{}' 
+           GROUP BY country
+           ORDER BY SUM(ecContribution) DESC'''
+
+df_participants = pd.read_sql_query(new_participants, conn)
+df_participants = pd.read_sql_query("""SELECT * FROM participants WHERE country = '{}' """.format(selectedacronym), conn)
+
+
+conn.close()
+print(df_participants)
+
+#2.9 Visualization of the new dataframe
+st.dataframe(df_participants) 
+
+#2.10  Generating a project coordinators dataframe
+
+conn = sqlite3.connect('ecsel_database.db')
+new_coordinators = '''SELECT country, shortName, name, projectAcronym, activityType, role 
+           FROM participants
+           WHERE role = 'coordinator' AND 'country'='{}' 
+           GROUP BY country
+           ORDER BY shortName ASC'''
+df_coordinators = pd.read_sql_query("""SELECT * FROM participants WHERE country = '{}' """.format(selectedacronym), conn)
+
+
+conn.close()
+
+print(df_coordinators)
+
+#2.11 Visualization of the project coordinators dataframe
+st.dataframe(df_coordinators) 
+                   
